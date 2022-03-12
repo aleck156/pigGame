40afd8fcada9605875a0ab19d5d730ebc143c533
Aleksander Jaworski <aleck156@gmail.com>
2022-03-12 11:08:39 +0100

    rename variable names to match the logic

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 'use strict';
 
-const score1 = document.querySelector('#score--0');
-const score2 = document.querySelector('#score--1');
+const score0 = document.querySelector('#score--0');
+const score1 = document.querySelector('#score--1');
