5eb45337361a8775c7ec66b80f51a131e5de6799
Aleksander Jaworski <aleck156@gmail.com>
2022-03-13 07:04:32 +0100

    grab player 0 and 1 elements from document

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const score1El = document.querySelector('#score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 const diceEl = document.querySelector('.dice');
 
 const btnRollDice = document.querySelector('.btn--roll');
