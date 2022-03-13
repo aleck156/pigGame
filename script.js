fa24f1815ea08c617262e542798e0e2d9ec71a3f
Aleksander Jaworski <aleck156@gmail.com>
2022-03-13 06:09:15 +0100

    grab all 3 buttons

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const diceEl = document.querySelector('.dice');
 
+const btnRollDice = document.querySelector('.btn--roll');
+const btnNewGame = document.querySelector('.btn--new');
+const btnHoldGame = document.querySelector('.btn--hold');
+
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
@@ -11,3 +15,7 @@ diceEl.classList.add('hidden');
 const rollDice = function () {
   return Math.floor(Math.random() * 6) + 1;
 };
+
+btnRollDice.addEventListener('click', () => {
+  console.log(rollDice());
+});
